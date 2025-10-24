3bece99dd45fb2c49f20fc416ed737aeeb8db897
Alvaro Garcia <agarram0711@g.educaand.es>
2025-10-24 21:22:53 +0200

    Muchas cosas + tareas

---
+++ Unidad02/practica/Ejercicio3.js
@@ -0,0 +1,8 @@
+let edad = prompt("¿Cuantos años tienes?");
+let esMayor = false;
+if (edad >= 18) {
+    esMayor = true;
+    console.log("Eres mayor de edad");
+} else {
+    console.log("Eres menor");
+}
