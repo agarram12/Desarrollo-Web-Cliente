df80f61401445fddd3d87b7f86f5d51d3320f96e
Alvaro Garcia <agarram0711@g.educaand.es>
2025-10-30 16:41:51 +0100

    Actividades de la práctica pasada y nuevos ejercicios de array

---
+++ Unidad02/practica/Ejercicio7.js
@@ -0,0 +1,7 @@
+var base = 15;
+var altura = 20;
+var area = base * altura;
+var perimetro = 2 * (base + altura);
+
+console.log(area);
+console.log(perimetro);
---
+++ Unidad02/practica/Ejercicio8.js
@@ -0,0 +1,4 @@
+let nr1 = 2;
+let nr2 = "2";
+console.log(nr1 == nr2);
+console.log(nr1 === nr2);
